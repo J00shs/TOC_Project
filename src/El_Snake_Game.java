fb4e22f42b50e1b344f14f8325547b1b47d43586
josh <j.surujpaul22@gmail.com>
2023-04-05 13:58:22 -0400

    init

---
+++ src/El_Snake_Game.java
@@ -0,0 +1,6 @@
+public class El_Snake_Game {
+    public static void main(String[] args) {
+        //Creates an instance of a window.
+     new GameWindow();
+    }
+}
---
+++ src/GameWindow.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+/*
+GameFrame defines the WINDOW of the game.
+It uses the JFrame Library.
+ */
+public class GameWindow extends JFrame {
+    /*
+    Constructor
+     */
+    GameWindow(){
+       this.add(new GamePanel());
+       this.setTitle("Snake"); //Sets the title of the Window
+       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //When the exit button is clicked, the application will end.
+       this.setResizable(true); //Allows the window to be resized by the user. If false, the window will not be resizable.
+       this.pack(); //Ensures that all contents within the window is sized correctly.
+       this.setVisible(true); //Makes the window visible to the user.
+       this.setLocationRelativeTo(null);//Centers the window to the middle of the screen.
+    }
+}
