6c9519955c25233820066dcecf8a4786a335ef31
Your Name <j.surujpaul22@gmail.com>
2023-04-10 16:31:40 -0400

    init

---
+++ src/gameCode.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+/*
+Class containing the game code
+ */
+
+public class gameCode extends JPanel {
+    public gameCode(){
+
+
+        JLabel label = new JLabel("This is where the code for the game will go.");
+        this.add(label);
+    }
+}
